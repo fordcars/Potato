c12ada044e7d990fac5250b04bc4d2f25951256f
fordcars <corteocarl@gmail.com>
2014-12-22 12:16:09 -0500

    Update gamejolt.js

--- src/gamejolt.js
+++ src/gamejolt.js
@@ -1,5 +1,5 @@
 // Potato
-// Copyright © 2014 Carl Hewett
+// Copyright Â© 2014 Carl Hewett
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -13,6 +13,8 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// NOTE: REQUIRES md5.js
 
 function getSignature(url, gamePrivateKey)
 {
