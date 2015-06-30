b95eee9009b70d71ce27ef30ee0c01e15c462cf5
fordcars <corteocarl@gmail.com>
2015-06-30 01:42:41 -0400

    Version 0.4

--- src/main.js
+++ src/main.js
@@ -232,7 +232,7 @@ function definitions() // Has access to other functions, since this is after fun
 	
 	// Constants
 	c.gameName = "Potato";
-	c.gameVersion = "0.3";
+	c.gameVersion = "0.4";
 	c.numberOfLayers = 3;
 	c.defaultCanWidth = 800;
 	c.defaultCanHeight = 600;
